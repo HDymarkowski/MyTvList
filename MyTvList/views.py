50a6d365abcc43c6b86968148542b75146634df3
HDymarkowski <2468988d@student.gla.ac.uk>
2021-04-05 16:00:30 +0100

    added Watchlist page

--- MyTvList/views.py
+++ MyTvList/views.py
@@ -17,19 +17,19 @@ def index(request):
     context_dict = {}
     context_dict['popular'] = tmdbSimpleApi.getPopular(1)
     context_dict['popular']['imgFile'] =tmdbSimpleApi.img(context_dict['popular']['poster_path'])
+    
+    if request.user.is_authenticated:
+        Username = request.user
 
-    Username = request.user
+        profile = UserProfile.objects.get(pk = Username.id)
+        UserFavouriteShow = profile.favourite_Show_Name
 
-    profile = UserProfile.objects.get(pk = Username.id)
-    UserFavouriteShow = profile.favourite_Show_Name
-
-    context_dict['recs'] = tmdbSimpleApi.getRecommendations(UserFavouriteShow, 3)
+        context_dict['recs'] = tmdbSimpleApi.getRecommendations(UserFavouriteShow, 3)
 
 
-    for rec in context_dict['recs']:
-        rec['imgFile'] = tmdbSimpleApi.img(rec['poster_path'])
+        for rec in context_dict['recs']:
+            rec['imgFile'] = tmdbSimpleApi.img(rec['poster_path'])
 
-    print(context_dict)
 
     response = render(request, 'Homepage.html', context=context_dict)
 
